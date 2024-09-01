fed6df2df74e4d6657a5cc1b1118d7cb133d85e1
Serhii Smirnov <snoff87@gmail.com>
2024-09-01 12:55:03 +0300

    lesson 6 in progress (added get_operations)

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import get_async_session
+from operations.models import operation
 
 router = APIRouter(
-    prefix = "/operations",
-    tags = ["Operation"]
+    prefix="/operations",
+    tags=["Operation"]
 )
 
+
 @router.get("/")
-async def get_operations():
-    return
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(operation).where(operation.c.type == operation_type)
+    result = await session.execute(query)
+    return result.mappings().all()
